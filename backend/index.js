8820e1432faf6ef91187a24295a31f570395fa7f
markbrutx <themarkbrut@gmail.com>
2023-04-17 21:38:30 +0600

    add backend core

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const req = await axios.put("http://api.chatengine.io/users", {
+        username: username, secret: username
+    },
+    {headers: {"private-key": "af04ffea-3ef0-422d-af32-ae998deefdcb"}}
+    )
+    return res.status(req.status).json(req.data);
+    }
+   catch (err) {
+    console.error(err);
+    return res.status(err.response.status).json(err.response.data);
+  }
+});
+
+app.listen(3001);
